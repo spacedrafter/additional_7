4dfd0e39edfd1a6f49d5d7d2cb37671170fd4c5e
Dmitry Shaido <d.shaido@gmail.com>
2018-02-28 15:01:27 +0300

    task 9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    solve(matrix);
+    return matrix;
+}
+
+function solve(matrix) {
+  for (var row = 0; row < 9; row++) {
+    for (var column = 0; column < 9; column++) {
+      if (matrix[row][column] === 0) {
+        for (var number = 1; number <= 9; number++) {
+          if (checkIndexes(matrix, row, column, number) && checkSquares(matrix, row, column, number)) {
+            matrix[row][column] = number;
+            if (solve(matrix)) { 
+              return true; 
+            } else {
+              matrix[row][column] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return matrix;
+}
+
+function checkSquares(matrix, row, column, number) {
+  var squareRow = Math.floor((row / 3)) * 3;
+  var squareColumn = Math.floor((column / 3)) * 3;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      if ((i != row) && (j != column) && (matrix[squareRow + i][squareColumn + j] == number)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function checkIndexes(matrix, row, column, number) {
+  for (var i = 0; i < 9; i++) {
+    if ((i != row) && (matrix[i][column] == number)) {
+      return false;
+    }
+  }
+  for (var j = 0; j < 9; j++) {
+    if ((j != column) && (matrix[row][j] == number)) {
+      return false;
+    }
+  }
+  return true;
 }
